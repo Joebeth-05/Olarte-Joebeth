6750b750d7c08148ae89a37497fcaeae6284bd7d
Joebeth-05 <163217872+Joebeth-05@users.noreply.github.com>
2024-03-13 12:30:07 +0800

    Olarte
    Output

---
+++ resources/views/enroll/index.blade.php
@@ -0,0 +1,156 @@
+@extends('layouts.master')
+@section('breadcrumbs')
+<ul class="navbar-nav mr-lg-2">
+    <li class="nav-item ml-0">
+        <h4 class="mb-0">Enrollees</h4>
+    </li>
+    <li class="nav-item">
+        <div class="d-flex align-items-baseline">
+            <p class="mb-0">KCCF SMS</p>
+            <i class="typcn typcn-chevron-right"></i>
+            <p class="mb-0">Enrollees</p>
+        </div>
+    </li>
+</ul>
+@endsection
+@section('content')
+
+<div class="row">
+    <div class="col-md-12">
+        <div class="card">
+            <div class="table-responsive pt-3">
+                <table class="table table-striped project-orders-table">
+                    <thead>
+                        <tr>
+                            <th style="width: 2px;">No.</th>
+                            <th style="width:450px;">Full Name</th>
+                            <th>Present Address</th>
+                            <th>Contact No.</th>
+                            <th>Course</th>
+                            <th style="width: 140px;">Status</th>
+                        </tr>
+                        </thead>
+                    <tbody>
+                        <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>Joebeth D. Olarte/td>
+                                <td>Nasugbu Batangas</td>
+                                <td>09367445113</td>
+                                <td>BSIT</td>
+                                <td style="display: block; width: 150px;">
+                                  <input type="submit" style="width: 70px; height: 30px; background-color: bluegreen;" value="Approve" data-id="#" />
+                                  <input type="submit" style="width: 70px; height: 30px; background-color: emerald-green;" value="Reject" data-id="#" />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Content 2</td>
+                                <td>Content 2</td>
+                                <td>Content 2</td>
+                                <td>Content 2</td>
+                                <td>Content 2</td>
+                                <td style="display: block; width: 50px;">
+                                  <input type="submit" style="width: 70px; height: 30px; background-color: bluegreen;" value="Approve" data-id="#" />
+                                  <input type="submit" style="width: 60px; height: 25px; background-color: emerald-green;" value="Reject" data-id="#" />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Content 3</td>
+                                <td>Content 3</td>
+                                <td>Content 3</td>
+                                <td>Content 3</td>
+                                <td>Content 3</td>
+                                <td style="display: block; width: 150px;">
+                                  <input type="submit" style="width: 70px; height: 30px; background-color: bluegreen;" value="Approve" data-id="#" />
+                                  <input type="submit" style="width:70px; height:30px; background-color: emerald-green;" value="Reject" data-id="#" />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Content 4</td>
+                                <td>Content 4</td>
+                                <td>Content 4</td>
+                                <td>Content 4</td>
+                                <td>Content 4</td>
+                                <td style="display: block; width: 140px;">
+                                  <input type="submit" style="width: 70px; height: 30px; background-color: bluegreen;" value="Approve" data-id="#" />
+                                  <input type="submit" style="width: 60px; height: 25px; background-color: emerald-green;" value="Reject" data-id="#" />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Content 5</td>
+                                <td>Content 5</td>
+                                <td>Content 5</td>
+                                <td>Content 5</td>
+                                <td>Content 5</td>
+                                <td style="display: block; width: 150px;">
+                                  <input type="submit" style="width: 70px; height: 30px; background-color: bluegreen;"  value="Approve" data-id="#" />
+                                  <input type="submit" style="width: 70px; height: 30px; background-color: emerald-green;" value="Reject" data-id="#" />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Content 6</td>
+                                <td>Content 6</td>
+                                <td>Content 6</td>
+                                <td>Content 6</td>
+                                <td>Content 6</td>
+                                <td style="display: block; width: 150px;">
+                                  <input type="submit" style="width: 70px; height: 30px; background-color: bluegreen;"  value="Approve" data-id="#" />
+                                  <input type="submit" style="width: 70px; height: 30px; background-color: emerald-green;"  value="Reject" data-id="#" />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Content 7</td>
+                                <td>Content 7</td>
+                                <td>Content 7</td>
+                                <td>Content 7</td>
+                                <td>Content 7</td>
+                                <td style="display: block; width: 150px;">
+                                  <input type="submit" style="width: 70px; height: 30px; background-color: bluegreen;" value="Approve" data-id="#" />
+                                  <input type="submit" style="width: 70px; height: 30px; background-color: emerald-green;" value="Reject" data-id="#" />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Content 8</td>
+                                <td>Content 8</td>
+                                <td>Content 8</td>
+                                <td>Content 8</td>
+                                <td>Content 8</td>
+                                <td style="display: block; width: 150px;">
+                                  <input type="submit" style="width: 70px; height: 30px; background-color: bluegreen;" value="Approve" data-id="#" />
+                                  <input type="submit" style="width: 70px; height: 30-px; background-color: emerald-green;" value="Reject" data-id="#" />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Content 9</td>
+                                <td>Content 9</td>
+                                <td>Content 9</td>
+                                <td>Content 9</td>
+                                <td>Content 9</td>
+                                <td style="display: block; width: 150px;">
+                                  <input type="submit" style="width: 70px; height: 30px; background-color:bluegreen;" value="Approve" data-id="#" />
+                                  <input type="submit" style="width: 70px; height: 30px; background-color: emerald-green;"  value="Reject" data-id="#" />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Content 10</td>
+                                <td>Content 10</td>
+                                <td>Content 10</td>
+                                <td>Content 10</td>
+                                <td>Content 10</td>
+                                <td style="display: block; width: 150	px;">
+                                  <button  class="btn btn-sm btn-success" ><i class="typcn typcn-tick-outline"></i></button>
+                                  <button class="btn btn-sm btn-danger"><i class="typcn typcn-times-outline"></i></button>
+                                </td>
+                            </tr>
+                            <tbody>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+@section('css')
+@endsection
+@section('script')
+@endsection
